85dde09416f9c355a0858d7b228d04f7b436feeb
Ding, Ran <kevinding0218@gmail.com>
2020-01-13 23:56:06 -0500

    creating a functional component

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// a component is just a function which returns some jsx, some html
+const person = () => {
+    return <p>I'm a Person!</p>
+};
+
+// export the function as default of this file
+export default person;
