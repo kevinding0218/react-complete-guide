05242a0f40c28353842a7d3f88277d0ea9b5b63a
Ding, Ran <kevinding0218@gmail.com>
2020-01-14 00:10:00 -0500

    children properties

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 // a component is just a function which returns some jsx, some html
 // props means the attributes on your component, e.g: <Person name="Max" age="28" />
+// props.children refers to any elements that's between the opening and closing tag of our component
 const person = (props) => {
-return <p>I'm {props.name} and I am {props.age} years old!</p>
+return (
+<div>
+    <p>I'm {props.name} and I am {props.age} years old!</p>
+    <p>{props.children}</p>
+</div>)
 };
 
 // export the function as default of this file
