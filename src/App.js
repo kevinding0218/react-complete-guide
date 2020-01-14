d0da3ad7e390d07d3d0ca173d47d33b2d5207254
Ding, Ran <kevinding0218@gmail.com>
2020-01-13 23:38:59 -0500

    understand JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // it does look like html but it is NOT
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+
+    // param1: element that you want to render in the DOM, e.g: html or your own component
+    // param2: (optional) configuration for param1 element, e.g: css class name
+    // param3: any children element which is nested inside the param1 element
+    // param4: content of param3 element
+    // html render as
+    // <div>"h1" "Hi, I'm a React App!!!"
+    // 'h1' here is interpreted as text, not render as an element
+    // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
+    
+    // if we want to render another element inside div, we need
+    return React.createElement('div', {className: 'App'}, 
+      React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
