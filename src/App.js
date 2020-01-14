bcf1e978734a45a427334591f8ad30b6f288e50d
Ding, Ran <kevinding0218@gmail.com>
2020-01-13 23:49:05 -0500

    JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 class App extends Component {
   render() {
     // it does look like html but it is NOT
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
+    // all the 'div'/'h1' are not html element but managed or provided by the react
+    // note here the className attribute was defined in JSX but will later on be translated to class in HTML
+
+    // it is a typical thing and typical best practice to wrap everything into one root element per component
+    // also makes sense since you typically want to style your components and want to add css class
+    // which is responsible for styling to the root element as <div>.
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+      </div>
+    );
 
     // param1: element that you want to render in the DOM, e.g: html or your own component
     // param2: (optional) configuration for param1 element, e.g: css class name
@@ -20,8 +27,8 @@ class App extends Component {
     // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!');
     
     // if we want to render another element inside div, we need
-    return React.createElement('div', {className: 'App'}, 
-      React.createElement('h1', null, 'Does this work now?'));
+    // return React.createElement('div', {className: 'App'}, 
+    //   React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
