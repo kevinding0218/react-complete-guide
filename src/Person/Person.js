057cd06e2d7d7a30dc03e974fff32b9b3432104f
Ding, Ran <kevinding0218@gmail.com>
2020-01-14 00:06:08 -0500

    working with props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 // a component is just a function which returns some jsx, some html
-const person = () => {
-    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+// props means the attributes on your component, e.g: <Person name="Max" age="28" />
+const person = (props) => {
+return <p>I'm {props.name} and I am {props.age} years old!</p>
 };
 
 // export the function as default of this file
