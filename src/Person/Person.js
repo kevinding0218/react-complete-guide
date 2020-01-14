0a897cc5b9e3e38293e026add841ace3f02d8d29
Ding, Ran <kevinding0218@gmail.com>
2020-01-14 00:02:05 -0500

    output dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // a component is just a function which returns some jsx, some html
 const person = () => {
-    return <p>I'm a Person!</p>
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 };
 
 // export the function as default of this file
